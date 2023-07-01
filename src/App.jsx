97649ccf3811b2a5779f09975c23511676854bc9
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-07-01 18:41:12 +0530

    ✨ feat: Add functionality to handle the search to fetch number of users.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import { useState } from 'react';
 import './App.css';
+
+import { Container } from '@mui/material';
+
+import InputField from './Components/InputField';
+
+import axios from 'axios';
 
 function App() {
 
+  const [usersNum, setUsersNum] = useState("");
+
+  const handleSearch = async () => {
+    // console.log(usersNum);
+    const response = await axios.get(`https://randomuser.me/api/?results=${usersNum}`);
+    console.log(response.data.results);
+    setUsersNum("");
+  }
+
   return (
-    <div>
-      <h1>Dating App</h1>
-    </div>
+    <Container maxWidth="lg">
+      <InputField usersNum={usersNum} setUsersNum={setUsersNum} handleSearch={handleSearch} />
+    </Container>
   )
 }
 
